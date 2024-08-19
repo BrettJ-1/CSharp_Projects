1243f9cedcbaf09f9192629fb96cce13dc3816f2
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-19 13:37:50 -0700

    "Hello, World!" in C#
    All course content up to welcome screen

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    internal class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Acme Accounting Systems \n \"Remember, we're \" accounting \" on you!");
+        Console.ReadLine();
+        }
+    }
+
---
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace namespacemyConsoleProjects
+{
+    class Program
+    {
+        static void Main(Console.WriteLine("What is your name?"))
+
