1651bb845633550a0a6ed8e20a746a09f40fe1ca
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-20 16:42:15 -0700

    Fixed Errors in BooleanLogicAssignment

--- BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -11,19 +11,21 @@ namespace BooleanLogicAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("What is your age?"); //Checks for age
-            int Age = Console.ReadLine().Length;
+            int Age = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Have you ever had a DUI?"); //Checks for DUI
             string Response = Console.ReadLine();
             bool DUI = (Response == "yes");
 
             Console.WriteLine("How many speeding tickets do you have?"); //Checks for speeding tickets
-            int Tickets = Console.ReadLine().Length;
+            int Tickets = Convert.ToInt32(Console.ReadLine());
 
             bool Qualified = (Age > 15) && DUI && (Tickets <= 3); //Checks if all three qualifications are met
 
             Console.WriteLine("Qualified?"); //Takes checked qualifications and displays boolean result
             Console.WriteLine(Qualified);
+
+            Console.ReadLine(); //Ends program
 
         }
     }
