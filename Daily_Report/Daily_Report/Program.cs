7cd654692d41ddd7362581fdf19b0a12c2215ba6
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-19 17:01:21 -0700

    Updated Daily Report
    Finished daily report questions

--- Daily_Report/Daily_Report/Program.cs
+++ Daily_Report/Daily_Report/Program.cs
@@ -12,7 +12,7 @@ internal class Program
     string Name; //Creates string 
     string Course; //Creates string 
     int PageNumber; //Creates integer 
-    bool NeedsHelp; //Creates boolean 
+    string NeedsHelp; //Creates boolean 
     string Experience; //Creates string 
     string Feedback; //Creates string 
     double StudyHours; //Creates double 
@@ -22,19 +22,25 @@ internal class Program
         Console.WriteLine("Student Daily Report"); //Creates Text Line
 
         Console.WriteLine("A. What is your name?"); //Creates Text Line
-        Console.WriteLine(Program.Name);
+        string Name = Console.ReadLine(); //User can input string
 
         Console.WriteLine("B. What course are you on?"); //Creates Text Line
+        string Course = Console.ReadLine(); //User can input string
 
         Console.WriteLine("C. What page number?"); //Creates Text Line
+        int PageNumber = Console.ReadLine().Length; //User can input integer
 
-        Console.WriteLine("D. Do you need help with anything ? Please answer \"true\" or \"false\". "); //Creates Text Line
+        Console.WriteLine("D. Do you need help with anything ? Please answer \"true\" or \"false\"."); //Creates Text Line
+        string NeedsHelp = Console.ReadLine(); //User can input string that gets converted to a boolean output
 
         Console.WriteLine("E. Were there any positive experiences you'd like to share? Please give specifics."); //Creates Text Line
+        string Experience = Console.ReadLine(); //User can input string
 
         Console.WriteLine("F. Is there any other feedback you'd like to provide? Please be specific."); //Creates Text Line
+        string Feedback = Console.ReadLine(); //User can input string
 
         Console.WriteLine("G. How many hours did you study today?"); //Creates Text Line
+        double StudyHours = Console.ReadLine().Length; //User can input double
 
         Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!"); //Creates Text Line
         Console.ReadLine(); //Prints Text Lines
