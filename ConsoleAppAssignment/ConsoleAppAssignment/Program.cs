be05e484869fa5592458f03923d2e3b1bcfe3b61
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-20 21:22:08 -0700

    Worked on ConsoleAppAssignment
    Created a do while loop in C#

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -11,37 +11,52 @@ namespace ConsoleAppAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Would you like to hear current music?"); //Asks user a question
+            string response;
+
+            do
+            {
+                Console.WriteLine("Would you like to hear current music? (yes/no)"); // Asks user a question
+                response = Console.ReadLine();
+
+                if (response == "yes") // Provides current music suggestions
+                {
+                    Console.WriteLine("Great! Here are three of the top hits from August 2024:");
+                    Console.WriteLine("1. Not Like Us");
+                    Console.WriteLine("2. I Had Some Help (ft. Morgan Wallen)");
+                    Console.WriteLine("3. Espresso");
+                }
+                else if (response == "no") // Doesn't provide music suggestions
+                {
+                    Console.WriteLine("Okay, I won't provide any recommendations for current music. Have a great day!");
+                }
+                else // Asks user to keep within specific guidelines
+                {
+                    Console.WriteLine("Please answer with 'yes' or 'no'");
+                }
+                Console.ReadLine();
+
+            } while (response != "yes" && response != "no"); // Continues loop until user provides a valid response
+
             while (true)
             {
-                string response = Console.ReadLine();
+                Console.WriteLine("Would you like to hear 80's music? (yes/no)");
+                response = Console.ReadLine();
 
+                if (response == "yes")
                 {
-                    if (response == "yes") //Provides current music suggestions
-                    {
-                        Console.WriteLine("Great! Here are three of the top hits from August 2024:");
-
-                        Console.WriteLine("1. Not Like Us");
-
-                        Console.WriteLine("2. I Had Some Help (ft. Morgan Wallen)");
-
-                        Console.WriteLine("3. Espresso");
-
-                        Console.ReadLine();
-                    }
-                    else if (response == "no") //Doesn't provide music suggestions
-                    {
-                        Console.WriteLine("Okay, I won't provide any recommendations for current music. Have a great day!");
-
-                        Console.ReadLine();
-                    }
-                    else //Asks user to keep within specific guidelines
-                    {
-                        Console.WriteLine("Please answer with a 'yes' or 'no'");
-
-                        Console.ReadLine();
-                    }
+                    Console.WriteLine("Great! I would recommend 'Livin on a Prayer' by Bon Jovi");
                 }
+
+                else if (response == "no")
+                {
+                    Console.WriteLine("Okay, I won't provide any recommendations for 80's music. Have a great day!");
+                }
+                else
+                {
+                    Console.WriteLine("Please answer with 'yes' or 'no'");
+                }
+                Console.ReadLine();
+                break;
             }
         }
     }
