574c11f8a50a763e7f71fb1df023c9656170511a
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-26 23:52:13 -0700

    Updated code
    Updated code from ConsoleAppArrayAssignment

--- ConsoleAppArrayAssignment/ConsoleAppArrayAssignment/Program.cs
+++ ConsoleAppArrayAssignment/ConsoleAppArrayAssignment/Program.cs
@@ -10,15 +10,60 @@ namespace ConsoleAppArrayAssignment
     {
         static void Main(string[] args)
         {
-            string[] Array = { "Car", "Truck", "Airplane", "Boat" };
-            Console.WriteLine("Choose a number between 0 and 3");
+            string[] stringArray = { "Car", "Truck", "Airplane", "Boat" }; //Creates index of vehicles
+            Console.WriteLine("Choose a number between 0 and 3 to select a vehicle");
 
-            string Input =
+            string input =
             Console.ReadLine();
-            int Index;
+            int index = Convert.ToInt32(input); //Converting int to string
 
-            Console.WriteLine("You selected" + Input);
+            if (index >= 0 && index <= 3)
+            {
+                string vehicle = stringArray[index];
+                Console.WriteLine("You selected a " + vehicle); //Output to user
+            }
+            else
+            {
+                Console.WriteLine("Please select a number between 0 and 3.");
+                Console.ReadLine();
+            }
+
+
+            int[] intArray = { 0, 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine("Double a number between 0 and 6");
+            string secondInput = Console.ReadLine();
+
+            int selectedNumber = Convert.ToInt32(secondInput);
+
+            if (selectedNumber >= 0 && selectedNumber <= 6)
+            {
+
+                int secondIndex = Convert.ToInt32(secondInput);
+
+                int result = intArray[secondIndex] * 2;
+
+                Console.WriteLine("Here is your result: " + result);
+
+            }
+            else
+            {
+                Console.WriteLine("Please select a number between 0 and 6.");
+                Console.ReadLine();
+            }
+
+            List<string> stringList = new List<string> { "Red", "Blue", "Green", "Yellow" };
+            Console.WriteLine("Select an index to display a color");
+
+            string userInput = Console.ReadLine();
+
+            int thirdIndex = Convert.ToInt32(userInput);
+
+            Console.WriteLine("You selected the color " + thirdIndex);
+            Console.ReadLine();
 
         }
+
     }
 }
+
+
