c96162261ae2439ed90e1b7ed03cf2275a5de5cd
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-29 13:24:28 -0700

    Updated Code

---
+++ MainMethodAssignment/MainMethodAssignment/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathOperations
+    {
+        public int Operation (int number)
+        {
+            return number + 5;
+        }
+        public int Operation(decimal number)
+        {
+            return (int)(number - 5);
+        }
+        public int Operation(string numberString)
+        {
+            int number = Convert.ToInt32(numberString);
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations ops = new MathOperations();
+
+            int intResult = ops.Operation(5);
+            Console.WriteLine(intResult);
+
+            int decimalResult = ops.Operation(12.34m);
+            Console.WriteLine(decimalResult);
+
+            int stringResult = ops.Operation("hello");
+            Console.WriteLine(stringResult);
+        }
+    }
+}
