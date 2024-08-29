3a3b888a4bde9c2c2e06e29b2e696b046b89c8eb
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-29 16:13:41 -0700

    Code Update

---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations ops = new Operations(); 
+            Console.WriteLine("Enter 2 numbers. You do not have to enter anything for the second number:"); //Takes user input for first number
+            string input = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(input);
+
+            Console.WriteLine("Enter the second number (or skip):"); //Takes user input for second number
+            input = Console.ReadLine();
+            int secondNumber = 0;
+            secondNumber = Convert.ToInt32(input); 
+            
+            int sum = ops.Add(firstNumber, secondNumber); //takes code from Operations.cs to add firstNumber and secondNumber
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+        }
+    }
+}
