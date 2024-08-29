e0b736f84940028a23414071b050bb81152435e3
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-29 12:09:07 -0700

    Updated Code

--- CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -11,13 +11,19 @@ namespace CallingMethodsAssignment
         static void Main(string[] args)
         {
             BasicMath math = new BasicMath();
-            Console.WriteLine("What number do you want to do math operations on?");
+            Console.WriteLine("What number do you want to do math operations on?"); //Asks user to input a number
 
             string input = Console.ReadLine();
             int number;
             number = Convert.ToInt32(input);
 
-            Console.WriteLine();
+            Console.WriteLine($"Adding 2: {math.AddTwo(number)}"); //Adds 2 from AddTwo in BasicMath
+            Console.ReadLine();
+
+            Console.WriteLine($"Subtracting 3: {math.SubtractThree(number)}"); //Subtracts 3 from SubtractThree in BasicMath
+            Console.ReadLine();
+
+            Console.WriteLine($"Dividing 4: {math.DivideFour(number)}"); //Divides 4 from DivideFour in BasicMath
             Console.ReadLine();
         }
     }
