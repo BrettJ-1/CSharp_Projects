619ec27ed5e58e5d42435b8dbb12bed6b4391517
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-29 11:21:34 -0700

    Draft Code

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/BasicMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class BasicMath
+    {
+        public int AddTwo(int number)
+        {
+            return number + 2;
+        }
+        public int SubtractThree(int number)
+        {
+            return number - 3;
+        }
+        public int DivideFour(int number)
+        {
+            return number / 4;
+        }
+                
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicMath math = new BasicMath();
+            Console.WriteLine("What number do you want to do math operations on?");
+
+            string input = Console.ReadLine();
+            int userNumber;
+            userNumber = Convert.ToInt32(input);
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
