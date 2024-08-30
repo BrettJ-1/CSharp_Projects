91914a95f5567ed34b7531b0bbf000180a16998c
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-08-29 19:11:17 -0700

    Code Update

---
+++ ClassSubmission/ClassSubmission/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public static class Operations
+    {
+        public static void division(int input, int output)
+        {
+            output = input / 2;
+        }
+        public static void division(double input, double output)
+        {
+            output = input / 2;
+        }
+    }
+    public class SecondOperations
+    {
+        public void VisibleDivision(int input)
+        {
+            int result = input / 2;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SecondOperations ops = new SecondOperations();
+            Console.WriteLine("Enter an integer");
+            int input = Convert.ToInt32(Console.ReadLine());
+            SecondOperations.VisibleDivision(input);
+            Console.ReadLine();
+        }
+    }
+}
