9ef91b3ce6c8666c3f0f81313bcdc973a8f6d63d
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-09-02 17:13:52 -0700

    Updated Code

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Employee : Person //Employee is inheriting from Person class
+    {
+        public int Id { get; set; } //Initializes int Id
+    }
+}
---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee
+            {
+                FirstName = "Sample", //Filling in variables from employee and Person class
+                LastName = "Student",
+                Id = 1
+            }; 
+            employee.SayName(); //Function from Person.cs
+            Console.ReadLine();
+        }
+    }
+}
