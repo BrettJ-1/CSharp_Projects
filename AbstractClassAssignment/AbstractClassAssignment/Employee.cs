70a02a3ec54aaaf7559998cc6fe459e971a4f5bb
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-09-02 17:59:27 -0700

    Updated Code

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+     class Employee : Person //Employee class is inherited from Person class
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name:" + firstName + " " + lastName);
+        }
+    }
+}
