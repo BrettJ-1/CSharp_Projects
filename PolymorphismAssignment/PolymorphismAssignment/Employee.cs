203952080ac2e2f7311466bb3a963b6380471b25
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-09-02 18:38:14 -0700

    Updated Code

---
+++ PolymorphismAssignment/PolymorphismAssignment/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    public class Employee : Person, IQuittable //Employee class is inheriting Person class and IQuittable interface
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name:" + firstName + " " + lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine(firstName + " " + lastName + " has quit.");
+        }
+    }
+}
---
+++ PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+            employee.SayName(); //Calling function from Person.cs
+            employee.Quit(); //Calling function from IQuittable.cs
+            Console.ReadLine();
+        }
+    }
+}
