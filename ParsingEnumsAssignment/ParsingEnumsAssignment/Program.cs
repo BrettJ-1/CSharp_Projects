635dbeb6e74d1c908ec989a8dd5f924b988fe802
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-09-03 11:13:02 -0700

    Updated Code

---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week.");
+            string input = Console.ReadLine();
+            try
+            {
+                daysOfWeek currentDay = (daysOfWeek)Enum.Parse(typeof(daysOfWeek), input, true); //Using Enum.Parse method
+                Console.WriteLine($"The day of the week is {currentDay}");
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Please enter an actual day of the week."); //catching any exceptions
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -6,12 +6,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new TwentyOneGame();
-            game.Players = new System.Collections.Generic.List<Player>();
-            Player player = new Player();
-            player.Name = "Jesse";
-            game += player;
-            game -= player;
 
             Deck deck = new Deck();
             deck.Shuffle(3);
