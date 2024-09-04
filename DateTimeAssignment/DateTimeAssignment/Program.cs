b46032345f56fa9845a5bdbbf8f72cd3bf40d654
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-09-03 22:20:31 -0700

    Commented Code

--- DateTimeAssignment/DateTimeAssignment/Program.cs
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -11,13 +11,13 @@ namespace DateTimeAssignment
         static void Main(string[] args)
         {
             DateTime currentDateTime = DateTime.Now;
-            Console.WriteLine("Current Date and Time:" + currentDateTime);
+            Console.WriteLine("Current Date and Time:" + currentDateTime); //Shows user the current date and time
             Console.WriteLine("Please enter a number");
             string input = Console.ReadLine();
             if (int.TryParse(input, out int hours))
             {
                 DateTime userDateTime = currentDateTime.AddHours(hours);
-                Console.WriteLine("The exact time in " + hours + " hours will be:" + userDateTime);
+                Console.WriteLine("The exact time in " + hours + " hours will be:" + userDateTime); //Concatenating variables and strings to show user future date and time
             }
             Console.ReadLine();
         }
