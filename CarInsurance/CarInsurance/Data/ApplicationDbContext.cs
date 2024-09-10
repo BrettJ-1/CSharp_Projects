bba1a4757b66d3f327c7349e6e98f793d973fdd0
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-09-10 01:01:15 -0700

    Updated Code

---
+++ CarInsurance/CarInsurance/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarInsurance.Models;
+
+namespace CarInsurance.Data
+{
+    public class QuoteContext : DbContext
+    {
+        public QuoteContext(DbContextOptions<
+            QuoteContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Insuree> Insurees { get; set; }
+    }
+}
--- CarInsurance/CarInsurance/obj/Debug/net8.0/CarInsurance.AssemblyInfo.cs
+++ CarInsurance/CarInsurance/obj/Debug/net8.0/CarInsurance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarInsurance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31e5c1bccf025b70932beff6ac9ff710995d056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60724c9188fef70bc29bb69ab6c940dc2e3e484c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarInsurance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarInsurance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
